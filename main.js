63173bdca8fa3f33d2f34712790b20d857e4797f
Jack Gerrits <jackgerrits95@gmail.com>
2019-02-28 23:24:59 -0500

    add splash page and session cookie

--- main.js
+++ main.js
@@ -277,7 +277,46 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {
+  document.cookie = name+'=; Max-Age=-99999999;';
+}
+
+function resetSessions()
+{
+  eraseCookie("sessions");
+}
+
 window.onload = async () => {
+  const sessions_container = document.getElementById("sessions");
+  if(getCookie("sessions") === null)
+  {
+    setCookie("sessions", "0");
+  }
+
+  let num_sessions = parseInt(getCookie("sessions"));
+  num_sessions += 1;
+  setCookie("sessions", num_sessions.toString());
+  sessions_container.innerHTML = num_sessions.toString();
+
   const info_time = document.getElementById("info-time");
   const info_location = document.getElementById("info-location");
   const info_weather = document.getElementById("info-weather");
